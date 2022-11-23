b92d93ce6bbbe3a1ed1b75d7a2e731ff0a054257
fortyguide <stefanopanico94@gmail.com>
2022-11-23 19:46:06 +0100

    Primo servizio REST + Configurazione DB H2

--- src/main/java/risorse/RisorsaHello.java
+++ src/main/java/risorse/RisorsaHello.java
@@ -2,12 +2,36 @@ package risorse;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/hello")
 public class RisorsaHello {
 
     @GET
+    @Produces({"text/plain"})
     public String saluto(){ //http://localhost:8080/Rubrica_REST/hello
-        return "Ciao, piacere di conoscerti!";
+        return "Ciao, piacere di conoscerti, da Paolo";
+    }
+
+    @GET
+    @Produces({"application/xml"})
+    public String salutoXML(){
+        return"<?xml version=\"1.0\" ?>\n" +
+              "\n" +
+              "<saluto>\n" +
+              "    <messaggio>Ciao, piacere di conoscerti</messaggio>\n" +
+              "    <da>Paolo</da>\n" +
+              "</saluto>";
+    }
+
+    @GET
+    @Produces({"application/json"})
+    public String salutoJSON(){
+        return "{\n" +
+                "\t\"saluto\": {\n" +
+                "    \"messaggio\": \"Ciao. piacere di conoscerti\"\n" +
+                "\t\t\"da\": \"Paolo\" \n" +
+                "    }\n" +
+                "}";
     }
 }
